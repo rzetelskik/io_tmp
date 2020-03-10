cc970359ccd20e5ce8c2177b885f10ce4d8c5d70
Kacper Rzetelski <kr406321@students.mimuw.edu.pl>
2020-03-10 21:01:57 +0100

    react setup

--- djangoapp/urls.py
+++ djangoapp/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app.views import FrontendAppView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^', FrontendAppView.as_view()),
 ]
