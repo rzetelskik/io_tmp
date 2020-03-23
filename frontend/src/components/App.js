6b4539ad6ddf7746241766dd167bb43158df6043
mkcin <marcin_kurowski@yahoo.pl>
2020-03-23 17:16:30 +0100

    cleaning

---
+++ frontend/src/components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <h1>
+      Nareszcie kurwa to jebane logo sie nie kreci i nie udajemy, że coś mamy
+    </h1>
+  );
+}
+
+export default App;
