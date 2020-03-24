33edb1d3fa52bf42afaa1312f8d416b7000b1232
mkcin <marcin_kurowski@yahoo.pl>
2020-03-24 20:48:30 +0100

    login debug

--- frontend/src/components/user/Dashboard.js
+++ frontend/src/components/user/Dashboard.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 export class Dashboard extends Component {
   render() {
     return (
       <div className="container">
         <h1>Jeśli to widzisz, jesteś zalogowany</h1>
+        <p>Twój token: {this.props.token}</p>
       </div>
     );
   }
 }
 
-export default Dashboard;
+const mapStateToProps = state => ({
+  token: state.auth.token
+});
+
+export default connect(mapStateToProps)(Dashboard);
