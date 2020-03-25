3bbd57eb3ff7b8b7960be82120f5da782c6bdb0c
Kacper Rzetelski <kr406321@students.mimuw.edu.pl>
2020-03-25 19:08:39 +0100

    backend tests - views and urls

--- account/api/views.py
+++ account/api/views.py
@@ -1,31 +1,31 @@
 from rest_framework import status, permissions, generics
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
-
 from knox.models import AuthToken
-
-from .serializers import CustomUserSerializer, RegisterSerializer, LoginSerializer, PasswordUpdateSerializer, DetailsUpdateSerializer
+from .serializers import CustomUserSerializer, RegisterSerializer, LoginSerializer, PasswordUpdateSerializer, \
+    DetailsUpdateSerializer
 from account.models import CustomUser
 
-@api_view(['POST',])
-def registerView(request):
+
+@api_view(['POST', ])
+def register(request):
     serializer = RegisterSerializer(data=request.data)
     response_data = {}
 
     serializer.is_valid(raise_exception=True)
     user = serializer.save()
-    token = AuthToken.objects.create(user)[1] # getting the actual token, not an object
+    token = AuthToken.objects.create(user)[1]  # getting the actual token, not an object
     response_data['response'] = 'Successfully created a new user.'
     response_data['token'] = token
 
     user_serializer = CustomUserSerializer(user)
     response_data['user'] = user_serializer.data
-    
+
     return Response(response_data)
 
 
 @api_view(['POST', ])
-def loginView(request):
+def login(request):
     serializer = LoginSerializer(data=request.data)
     response_data = {}
 
@@ -37,48 +37,45 @@ def loginView(request):
 
     user_serializer = CustomUserSerializer(user)
     response_data['user'] = user_serializer.data
-    
+
     return Response(response_data)
-        
+
 
 class CustomUserDetailView(generics.RetrieveAPIView):
-    permission_classes = [permissions.IsAuthenticated,]
+    permission_classes = [permissions.IsAuthenticated, ]
     serializer_class = CustomUserSerializer
 
     def get_object(self):
         return self.request.user
 
 
-@api_view(['PUT',])
+@api_view(['PUT', ])
 @permission_classes([permissions.IsAuthenticated])
-def passwordUpdateView(request):
+def password_update(request):
     serializer = PasswordUpdateSerializer(request.user, data=request.data)
     response_data = {}
 
     serializer.is_valid(raise_exception=True)
     serializer.save()
     response_data['response'] = 'Password successfully changed.'
-    
+
     return Response(response_data)
 
 
-@api_view(['PUT',])
+@api_view(['PUT', ])
 @permission_classes([permissions.IsAuthenticated])
-def detailsUpdateView(request):
+def details_update(request):
     serializer = DetailsUpdateSerializer(request.user, data=request.data)
     response_data = {}
 
     serializer.is_valid(raise_exception=True)
     serializer.save()
     response_data['response'] = 'Account details successfully updated.'
-   
-    return Response(response_data)
 
+    return Response(response_data)
 
 
 ########## Wypisuje wszystkich CustomUserów - do testów
 class CustomUserListView(generics.ListAPIView):
     serializer_class = CustomUserSerializer
     queryset = CustomUser.objects.all()
-
-
