d27dd204121b1499fcf7612104973dac4a6b40a0
mkcin <marcin_kurowski@yahoo.pl>
2020-04-28 00:44:34 +0200

    tag init

---
+++ frontend/src/components/user_interface/tags/TagsForm.js
@@ -0,0 +1,44 @@
+import React, { Component, Fragment } from "react";
+import { Modal, Button } from "react-bootstrap";
+
+export class Geolocator extends Component {
+  state = {
+    modalShow: true,
+  };
+
+  understood = () => {
+    this.setState({ modalShow: false });
+  };
+
+  render() {
+    const { modalShow } = this.state;
+    return (
+      <Fragment data-test="geolocator">
+        <Modal
+          show={modalShow}
+          onHide={this.understood}
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+              Allow us to find you!
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>
+              In order to let us find you, please accept the following browser
+              pop-up
+            </p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.understood}>Understood</Button>
+          </Modal.Footer>
+        </Modal>
+      </Fragment>
+    );
+  }
+}
+
+export default Geolocator; //
