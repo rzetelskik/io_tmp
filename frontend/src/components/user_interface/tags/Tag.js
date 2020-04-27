f3d60cbbc3941d08025a1ef4065077a984cafd69
Anq <ak406173@students.mimuw.edu.pl>
2020-04-28 01:32:56 +0200

    Tag component

---
+++ frontend/src/components/user_interface/tags/Tag.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+export class Tag extends Component {
+  static propTypes = {
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    selected: PropTypes.bool.isRequired,
+    clickTag: PropTypes.func.isRequired,
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <button
+          type="button"
+          onClick={this.props.clickTag(this.props.id)}
+          className={
+            this.props.id ? "btn btn-secondary" : "btn btn-outline-secondary"
+          }
+        >
+          {this.props.name}
+        </button>
+      </Fragment>
+    );
+  }
+}
+
+export default Tag;
