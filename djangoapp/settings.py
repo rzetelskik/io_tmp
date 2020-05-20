a42268761643d59123517327ff438d0932b60cf7
Kacper Rzetelski <32443348+rzetelskik@users.noreply.github.com>
2020-05-20 12:37:12 +0200

    Update settings.py
    (backend) enabled debug mode in djangoapp

--- djangoapp/settings.py
+++ djangoapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'f8pq51ei45b&$1365%o2h3jcwb*f8a7w@2sd-wjbd57k=g@7n$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']  # TODO change the wildcard to specific name
 
